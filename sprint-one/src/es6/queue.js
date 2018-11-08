6fe0753b9df55e1db7fdfbc4b364f31f2fe085c5
Jessica <jessicarhodson@gmail.com>
2018-11-07 16:26:25 -0800

    Implement es6 patter for queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.length = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  }
+
+  dequeue() {
+    var firstIn = this.storage[0];
+
+    if (this.length > 0) {
+      this.length--;
+    }
+
+    this.storage[0] = this.storage[1];
+
+    return firstIn;
+  }
+
+  size() {
+    return this.length;
+  }
 }
+
+var queue = new Queue();
